a47f1177d01d3a3576d46c49af84d15e1af6e408
meganlr <megan_power504@hotmail.com>
2019-02-17 22:17:24 +0000

    Changed to hashrouter

--- src/components/main.js
+++ src/components/main.js
@@ -4,16 +4,16 @@ import About from './aboutme';
 import Contact from './contact';
 import Projects from './projects';
 import CV from './CV';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 const Main = () => (
-    <Switch>
+    <Router>
         <Route exact path="/" component= {LandingPage} />
         <Route exact path="/projects" component= {Projects} />
         <Route exact path="/about" component= {About} />
         <Route exact path="/contact" component= {Contact} />
         <Route exact path="/CV" component= {CV} />
-    </Switch>
+    </Router>
 )
 
 export default Main;
